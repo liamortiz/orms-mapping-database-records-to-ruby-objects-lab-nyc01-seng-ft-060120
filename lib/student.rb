ddb2591d76c58f99da12c420be76e313f2166f56
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 22:25:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,8 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    
     student = self.new
-    student(:id, :name, :grade) = (row[0], row[1], row[2])
+    student.id, student.name, student.grade = row[0], row[1], row[2]
     student
     
   end
