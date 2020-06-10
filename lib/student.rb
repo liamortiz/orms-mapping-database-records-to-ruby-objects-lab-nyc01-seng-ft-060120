c3cc2cf6b5d103b70d195caa4e7bfbc070efe103
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 23:12:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,15 @@ class Student
     first_X_students_in_grade_10(1)[0]
   end
   
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      new_from_db(row)
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
