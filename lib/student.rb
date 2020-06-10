7fdd1a2077c4e2fb7548b4178687766b8a666edb
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 22:53:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,14 +31,14 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
     SQL
-    DB[:conn].execute(sql)[0]
+    DB[:conn].execute(sql)
   end
   
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students WHERE grade < 12
     SQL
-    p DB[:conn].execute(sql)[0]
+    p DB[:conn].execute(sql)
   end
   
   def save
