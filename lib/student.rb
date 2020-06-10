382e5bd5ff65a266df5118ad685eb707fe106323
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 23:13:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,64 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
+    
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    DB[:conn].execute("SELECT * FROM students").map do |row|
+      new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name)[0]
+    new_from_db(row)
+  end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    [new_from_db(DB[:conn].execute(sql)[0])]
+  end
+  
+  def self.first_X_students_in_grade_10(limit)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql, limit).map do |row|
+      new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1)[0]
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      new_from_db(row)
+    end
   end
   
   def save
@@ -29,7 +77,7 @@ class Student
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT
+      grade INTEGER
     )
     SQL
 
